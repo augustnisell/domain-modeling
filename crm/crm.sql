749a99fb46e3a9430c315e1b1a93ecc62ada5c11
augustnisell <79811628+augustnisell@users.noreply.github.com>
2023-01-17 22:29:59 +0000

    Practiced domain modeling

--- crm/crm.sql
+++ crm/crm.sql
@@ -1,5 +1,38 @@
 -- Drops existing tables, so we start fresh with each
 -- run of this script
 -- e.g. DROP TABLE IF EXISTS ______;
+DROP TABLE IF EXISTS salespersons;
+DROP TABLE IF EXISTS contacts;
+DROP TABLE IF EXISTS activities;
+DROP TABLE IF EXISTS companies;
 
 -- CREATE TABLES
+CREATE TABLE salespersons (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  first_name TEXT,
+  last_name TEXT,
+  email TEXT
+);
+
+CREATE TABLE contacts (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  first_name TEXT,
+  last_name TEXT,
+  email TEXT,
+  phone_number TEXT,
+  company_id TEXT
+);
+
+CREATE TABLE activities (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  date TEXT,
+  type TEXT,
+  notes TEXT,
+  salesperson_id INTEGER,
+  contact_id INTEGER
+);
+
+CREATE TABLE companies (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name TEXT
+);
--- tacostagram/tacostagram.sql
+++ tacostagram/tacostagram.sql
@@ -1,1 +1,45 @@
+-- As a user, I want to create time-stamped posts of tacos, so that I can share them in a "feed" for the world to see.
+-- As a user, I want to be able to "like" another user's post, so the author of the post and others will know that I liked it.
+-- As a user, I want to be able to see how many likes a post has, so I can see how popular it is.
+-- As a user, I want to not be able to like a post more than once, so I cannot artificially inflate the popularity of a post.
+-- As a user, I want to be able to comment (and see other's comments) on a post, so I can engage in conversation with the author and others.
+-- As a user, I want to be able to "follow" another user, so that I can stay up-to-date on certain users' updates.
+-- As a user, I want to see only my followed users' posts in my feed, so I can only see posts that I'm interested in.
+-- As a user, I want to sign up with my username (screen name), real name, and location, so that other people will see that information when visiting my profile.
 
+CREATE TABLE users (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  username TEXT,
+  real_name TEXT,
+  location TEXT
+);
+
+CREATE TABLE posts (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  user_id INTEGER,
+  image_url TEXT,
+  caption TEXT,
+  posted_at DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE likes (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  post_id INTEGER,
+  user_id INTEGER,
+  liked_at DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE comments (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  post_id INTEGER,
+  user_id INTEGER,
+  content TEXT,
+  commented_at DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE follows (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  follower_user_id INTEGER,
+  followee_user_id INTEGER,
+  followed_at DATETIME DEFAULT CURRENT_TIMESTAMP
+);
